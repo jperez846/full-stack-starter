27d16d33ae5ef3c36383af43afdff70132697484
Gabriel Cabrejas <gabriel@cabrejas.net>
2025-05-12 15:14:50 +0000

    path matching syntax

--- api/server.js
+++ api/server.js
@@ -1,3 +1,6 @@
+// Careful with your path matching syntax if you upgrade to Express 5
+// [Moving to Express 5](https://expressjs.com/en/guide/migrating-5.html#path-syntax)
+
 const path = require('path')
 const express = require('express')
 const helmet = require('helmet')
@@ -19,7 +22,7 @@ server.get('/api/friends', async (req, res, next) => {
   }
 })
 
-server.get('*', (req, res) => {
+server.get('/*splat', (req, res) => { // In Express 5 '*' won't work
   res.sendFile(path.resolve(__dirname, '..', 'frontend', 'dist', 'index.html'))
 })
 
