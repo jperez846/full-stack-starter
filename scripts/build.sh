eb614734c6a08b8921d941a7c544d79cfc79182d
jperez846 <jperez4032@gmail.com>
2025-05-14 20:06:16 +0000

    feat(kudos): revert

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,30 +1,11 @@
 #!/usr/bin/env bash
 
 # Install express deps
-# npm install
-
+npm install
 
 
 # Install react deps
-# cd frontend && npm install --include=dev
+cd frontend && npm install --include=dev
 
 # Build react app
-# npm run build
-#!/usr/bin/env bash
-set -e
-
-# Step 1: Install backend dependencies
-cd api
-npm install
-cd ..
-
-# Step 2: Generate Prisma client
-npx prisma generate
-
-# Optional: run DB migrations
-# npx prisma migrate deploy
-
-# Step 3: Install frontend dependencies and build
-cd frontend
-npm install --include=dev
 npm run build
